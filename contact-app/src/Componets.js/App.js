d3249b906b3b49bcf38e698a36ecdd7b5b87ac75
winfrom within <antonneturner@gmail.com>
2021-11-09 16:49:06 -0500

    import header, addContact, and contactList, create Header for addContact and form

--- contact-app/src/Componets.js/App.js
+++ contact-app/src/Componets.js/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
-
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ConctactList from "./ContactList";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <AddContact />
+      <ConctactList />
+    </div>
+  );
 }
 
 export default App;
